39e3745b14f10de312d468d793888437a850afa1
Vladimir Gordienko <vldmr.gordienko@gmail.com>
2018-08-25 18:57:17 +0300

    Add articles app

--- newspaper_project/urls.py
+++ newspaper_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
